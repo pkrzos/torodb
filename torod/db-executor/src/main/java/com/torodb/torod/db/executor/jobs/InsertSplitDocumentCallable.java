006b8e157375b8fa0a06c4da91ddb8970b3a715d
Jakub Wrabel <wrabel.jakub@gmail.com>
2014-12-08 00:04:29 +0100

    WET

--- torod/db-executor/src/main/java/com/torodb/torod/db/executor/jobs/InsertSplitDocumentCallable.java
+++ torod/db-executor/src/main/java/com/torodb/torod/db/executor/jobs/InsertSplitDocumentCallable.java
@@ -20,6 +20,26 @@
 
 package com.torodb.torod.db.executor.jobs;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import javax.annotation.Nonnull;
+
+import org.jooq.Configuration;
+import org.jooq.DSLContext;
+import org.jooq.Field;
+import org.jooq.InsertValuesStep2;
+import org.jooq.Record;
+import org.jooq.SQLDialect;
+import org.jooq.exception.DataAccessException;
+import org.jooq.impl.DSL;
+import org.jooq.impl.DefaultConfiguration;
+import org.jooq.impl.SQLDataType;
+
 import com.google.common.base.Function;
 import com.google.common.collect.ImmutableCollection;
 import com.google.common.collect.ImmutableList;
@@ -37,9 +57,8 @@ import com.torodb.torod.core.subdocument.SplitDocument;
 import com.torodb.torod.core.subdocument.SubDocType;
 import com.torodb.torod.core.subdocument.SubDocument;
 import com.torodb.torod.db.executor.DefaultSessionTransaction;
-import java.util.*;
-import java.util.concurrent.Callable;
-import javax.annotation.Nonnull;
+import com.torodb.torod.db.postgresql.meta.CollectionSchema;
+import com.torodb.torod.db.postgresql.meta.TorodbMeta;
 
 /**
  *
@@ -79,8 +98,38 @@ public class InsertSplitDocumentCallable implements Callable<InsertResponse> {
 
     private void insertSingleDoc(SplitDocument doc) throws ImplementationDbException, UserDbException {
         DbConnection connection = connectionProvider.getConnection();
-        connection.insertRootDocuments(collection, Collections.singleton(doc));
+//        connection.insertRootDocuments(collection, Collections.singleton(doc));
         
+        try {
+        	Configuration conf = new DefaultConfiguration()
+        	.set(SQLDialect.POSTGRES);
+        	DSLContext dslContext = DSL.using(conf);
+        	CollectionSchema colSchema =  new TorodbMeta(dslContext).getCollectionSchema(collection);
+
+            Field<Integer> idField = DSL.field("did", SQLDataType.INTEGER.nullable(false));
+            Field<Integer> sidField = DSL.field("sid", SQLDataType.INTEGER.nullable(false));
+          
+           
+            InsertValuesStep2<Record, Integer, Integer> insertInto = dslContext.insertInto(DSL.tableByName(colSchema.getName(), "root"), idField, sidField);
+
+            for (SplitDocument splitDocument : docs) {
+                int structureId = colSchema.getStructuresCache().getOrCreateStructure(splitDocument.getRoot(), dslContext);
+
+                insertInto = insertInto.values(splitDocument.getDocumentId(), structureId);
+            }
+
+            insertInto.execute();
+            
+        } catch (DataAccessException ex) {
+            //TODO: Change exception
+            throw new RuntimeException(ex);
+        } catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         for (SubDocType type : doc.getSubDocuments().rowKeySet()) {
             ImmutableCollection<SubDocument> subDocs = doc.getSubDocuments().row(type).values();
 
@@ -124,48 +173,71 @@ public class InsertSplitDocumentCallable implements Callable<InsertResponse> {
         DbConnection connection = connectionProvider.getConnection();
         List<WriteError> errors = Lists.newLinkedList();
         
-        try {
-            /*
-            * First we need to store the root documents
-            */
-            connection.insertRootDocuments(collection, docs);
-            
-            /*
-            * Then we have to store the subdocuments. It is more efficient to do one insert for each table, so inserts
-            * are done by subdocument type.
-            * To do that, we could create a map like Map<SubDocType, List<SubDocument>> and then iterate over the keys,
-            * but we need to duplicate memory and the documents to insert may be very big. So we decided to do it in a
-            * functional way. First we get all types and then we use an iterator that, for each type 't' and document 'd'
-            * does d.getSubDocuments().row(k).values().iterator and finally merges the iterators grouped by type.
-            */
-            Set<SubDocType> types = Sets.newHashSetWithExpectedSize(10 * docs.size());
-            for (SplitDocument splitDocument : docs) {
-                types.addAll(splitDocument.getSubDocuments().rowKeySet());
-            }
-            
-            /*
-            * The following code that uses guava functions is the same as the following jdk8 code:
-            * for (SubDocType type : types) {
-            *   java.util.function.Function<SplitDocument, Stream<SubDocument>> f = (sd) -> sd.getSubDocuments().row(type).values().stream();
-            *
-            *   Stream<SubDocument> flatMap = docs.stream().map(f).flatMap((stream) -> stream);
-            *
-            *   connection.insertSubdocuments(collection, type, flatMap.iterator());
-            *
-            * }
-            */
-            for (SubDocType type : types) {
-                Function<SplitDocument, Iterable<SubDocument>> extractor = new SubDocumentExtractorFunction(type);
-                
-                connection.insertSubdocuments(collection, type, Iterables.concat(Iterables.transform(docs, extractor)).iterator());
-            }
-            
-            return createResponse(docs.size(), errors);
-        } catch (UserDbException ex) {
-            appendError(errors, ex, 0);
-            connection.rollback();
-        }
-        return createResponse(0, errors);
+        /*
+		* First we need to store the root documents
+		*/
+//            connection.insertRootDocuments(collection, docs);
+		   Configuration conf = new DefaultConfiguration()
+		   .set(SQLDialect.POSTGRES);
+		   DSLContext dslContext = DSL.using(conf);
+		try {
+			CollectionSchema colSchema =  new TorodbMeta(dslContext).getCollectionSchema(collection);
+
+		    Field<Integer> idField = DSL.field("did", SQLDataType.INTEGER.nullable(false));
+		    Field<Integer> sidField = DSL.field("sid", SQLDataType.INTEGER.nullable(false));
+
+		    
+		    InsertValuesStep2<Record, Integer, Integer> insertInto = dslContext.insertInto(DSL.tableByName(colSchema.getName(), "root"), idField, sidField);
+
+		    for (SplitDocument splitDocument : docs) {
+		        int structureId = colSchema.getStructuresCache().getOrCreateStructure(splitDocument.getRoot(), dslContext);
+
+		        insertInto = insertInto.values(splitDocument.getDocumentId(), structureId);
+		    }
+
+		    insertInto.execute();
+		    
+		} catch (DataAccessException ex) {
+		    //TODO: Change exception
+		    throw new RuntimeException(ex);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		/*
+		* Then we have to store the subdocuments. It is more efficient to do one insert for each table, so inserts
+		* are done by subdocument type.
+		* To do that, we could create a map like Map<SubDocType, List<SubDocument>> and then iterate over the keys,
+		* but we need to duplicate memory and the documents to insert may be very big. So we decided to do it in a
+		* functional way. First we get all types and then we use an iterator that, for each type 't' and document 'd'
+		* does d.getSubDocuments().row(k).values().iterator and finally merges the iterators grouped by type.
+		*/
+		Set<SubDocType> types = Sets.newHashSetWithExpectedSize(10 * docs.size());
+		for (SplitDocument splitDocument : docs) {
+		    types.addAll(splitDocument.getSubDocuments().rowKeySet());
+		}
+		
+		/*
+		* The following code that uses guava functions is the same as the following jdk8 code:
+		* for (SubDocType type : types) {
+		*   java.util.function.Function<SplitDocument, Stream<SubDocument>> f = (sd) -> sd.getSubDocuments().row(type).values().stream();
+		*
+		*   Stream<SubDocument> flatMap = docs.stream().map(f).flatMap((stream) -> stream);
+		*
+		*   connection.insertSubdocuments(collection, type, flatMap.iterator());
+		*
+		* }
+		*/
+		for (SubDocType type : types) {
+		    Function<SplitDocument, Iterable<SubDocument>> extractor = new SubDocumentExtractorFunction(type);
+		    
+		    connection.insertSubdocuments(collection, type, Iterables.concat(Iterables.transform(docs, extractor)).iterator());
+		}
+		
+		return createResponse(docs.size(), errors);
     }
 
     private InsertResponse createResponse(int docInserted, @Nonnull List<WriteError> errors) {
